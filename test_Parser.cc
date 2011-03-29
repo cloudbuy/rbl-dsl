55a8b05b2b505cf75b54f7575a3c2141fefe92e2
Hassan.Syed <hassan.syed@ukplc.net>
2011-03-29 11:00:22 +0100

    data_name and identifier pair passing tests

--- test_Parser.cc
+++ test_Parser.cc
@@ -1,5 +1,34 @@
 #include "EventGrammarNew.h"
 #include <gtest/gtest.h>
+#include <string>
+
+typedef std::string::const_iterator c_s_it;
+
+using namespace event_model;
+
+TEST(parser_test, data_name)
+{
+    parser::Rules<c_s_it> rules;
+    
+    std::string data_name_string = "hasSan_Syed";
+    c_s_it beg = data_name_string.begin();
+    c_s_it end = data_name_string.end();
+    
+    bool res = boost::spirit::qi::parse(beg,end,rules.data_name);
+    ASSERT_TRUE(res);
+}
+
+TEST(parser_test, identifier_pair)
+{
+    parser::Rules<c_s_it> rules;
+    
+    std::string identifier_pair_string = "0:hassan";
+    c_s_it beg = identifier_pair_string.begin();
+    c_s_it end = identifier_pair_string.end();
+    
+    bool res = boost::spirit::qi::parse(beg,end,rules.identifier_pair);
+    ASSERT_TRUE(res);
+}
 
 int main(int argc,char ** argv)
 {
