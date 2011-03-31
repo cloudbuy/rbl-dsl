29573995d39abdebcf05ab2edfd704888cda74a5
Hassan.Syed <hassan.syed@ukplc.net>
2011-03-31 17:05:17 +0100

    this file should have been in a previous comit

---
+++ src/parser/Descriptors.cc
@@ -0,0 +1,34 @@
+#include "parser/Descriptors.h"
+
+namespace event_model
+{
+    using namespace primitives;
+    
+    void MarshallEventDescriptorBuilder::Init(   
+        const Oid & oid, 
+        MarshallNamespaceDescriptorBuilder & mndb,
+        bool & ok)
+    {
+        self_oid_ = oid;
+        mndb_= &mndb;
+        ok = true; 
+        
+        OP_RESPONSE resp = mndb_->ContainsEventIdentifier(oid);
+        
+        if(resp == OP_NO_ERROR) return;
+        //TODO set error string
+        ok = false;
+        return;
+    }
+    void MarshallEventDescriptorBuilder::AddEventType
+        (const Oid & oid, const EventTypeDescriptor & type, bool & ok)
+    {
+        ok=true;
+        if(types.ContainsEither(oid) == OP_NO_ERROR) {
+            EventTypeContainer::entry_type entry(oid,type);
+            types.SetEntry(entry);
+            return;
+        }
+        ok = false;
+    }
+};
