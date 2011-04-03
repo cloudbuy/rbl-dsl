5af8f635f6784df43bde6f26ed64b694de3ec271
hassan-home <h.a.syed@gmail.com>
2011-04-03 16:56:42 +0100

    unified the tests in descriptor building tests

--- ll_tests/parser/test_Descriptors.cc
+++ ll_tests/parser/test_Descriptors.cc
@@ -12,7 +12,7 @@ using namespace event_model;
 
 
 // test the serialization of the event type descriptor
-TEST(testing_const_serialization, test_one)
+TEST(descriptor_building_and_serialization, event_entry_serialization_test)
 {
     EventTypeDescriptor etd_default_constructed;
     EventTypeDescriptor etd_source(ENTRY_REQUIRED, VALUE_INT4, false);
@@ -29,8 +29,7 @@ TEST(testing_const_serialization, test_one)
     ASSERT_TRUE(etd_default_constructed.is_primitive() == false);
 }
 
-// 
-TEST(testing_marshall_building, test_one)
+TEST(descriptor_building_and_serialization, identifier_collision_tests)
 {
     MarshallNamespaceDescriptorBuilder mndb("testing");
     ASSERT_TRUE(mndb.name() == "testing");
@@ -66,8 +65,44 @@ TEST(testing_marshall_building, test_one)
     ASSERT_FALSE(res);
 //    medb.
 }
+// 
+TEST(descriptor_building_and_serialization, event_descriptor_building_test)
+{
+    MarshallNamespaceDescriptorBuilder mndb("testing");
+    ASSERT_TRUE(mndb.name() == "testing");
+    ASSERT_TRUE(mndb.events.size() == 0); 
+    ASSERT_TRUE(mndb.events.occupied_size() == 0);
 
-TEST(testing_marshall_building, test_two)
+    MarshallEventDescriptorBuilder medb;
+
+    bool res;
+    Oid hassan_zero("hassan",0);
+    Oid hassan_zero_ord_collision("hassan1",0);
+    Oid hassan_zero_name_collision("hassan",1);
+
+    medb.Init(hassan_zero,mndb,res);
+    ASSERT_TRUE(res);
+
+    medb.AddEventType(  hassan_zero,
+                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
+                        res);
+    ASSERT_TRUE(res);
+    medb.AddEventType(  hassan_zero,
+                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
+                        res);
+    ASSERT_FALSE(res);
+    
+    medb.AddEventType(  hassan_zero_ord_collision,
+                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
+                        res);
+    ASSERT_FALSE(res);
+    medb.AddEventType(  hassan_zero_name_collision,
+                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
+                        res);
+    ASSERT_FALSE(res);
+}
+
+TEST(descriptor_building_and_serialization, exhaustive_mndb_building_test)
 {
     MarshallNamespaceDescriptorBuilder mndb("testing");
     ASSERT_TRUE(mndb.name() == "testing");
@@ -188,6 +223,62 @@ TEST(testing_marshall_building, test_two)
     ASSERT_EQ(mndb.events[5]->types[8]->type(), VALUE_INT8);
     ASSERT_EQ(mndb.events[5]->types[8]->qualifier(),ENTRY_REQUIRED);
     ASSERT_EQ(mndb.events[5]->types[8]->is_primitive(), true);
+
+}
+
+TEST(descriptor_building_and_serialization, descriptor_downcast_slicing_tests)
+{
+    MarshallNamespaceDescriptorBuilder mndb("testing");
+    ASSERT_TRUE(mndb.name() == "testing");
+    ASSERT_TRUE(mndb.events.size() == 0); 
+    ASSERT_TRUE(mndb.events.occupied_size() == 0);
+
+    MarshallEventDescriptorBuilder medb1;
+    MarshallEventDescriptorBuilder medb2;
+    bool res;
+    Oid one("monkey",0);
+    Oid two("zebra",3);
+    Oid three("giraffe",8);
+    
+    medb1.Init(Oid("evenT",0),mndb,res);
+    ASSERT_TRUE(res);
+
+    medb1.AddEventType(  one,
+                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
+                        res);
+    ASSERT_TRUE(res);
+    medb1.AddEventType(  two,
+                        EventTypeDescriptor(ENTRY_OPTIONAL, VALUE_INT4, false),
+                        res);
+    ASSERT_TRUE(res);
+    
+    medb1.AddEventType(  three,
+                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT8, true),
+                        res);
+    ASSERT_TRUE(res);
+    
+    medb2.Init(Oid("monkey",5),mndb,res);
+    ASSERT_TRUE(res);
+
+    medb2.AddEventType(  one,
+                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
+                        res);
+    ASSERT_TRUE(res);
+    medb2.AddEventType(  two,
+                        EventTypeDescriptor(ENTRY_OPTIONAL, VALUE_INT4, false),
+                        res);
+    ASSERT_TRUE(res);
+    
+    medb2.AddEventType(  three,
+                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT8, true),
+                        res);
+    ASSERT_TRUE(res);
+
+    mndb.AddEventDescriptor(medb1,res);
+    ASSERT_TRUE(res);
+    
+    mndb.AddEventDescriptor(medb2,res);
+    ASSERT_TRUE(res);
 
     MarshallNamespaceDescriptor mnd = mndb;
     
@@ -397,44 +488,9 @@ TEST(testing_marshall_building, test_two)
     ASSERT_EQ(gnd.events[5]->types[8]->is_primitive(), true);
 }
 
-TEST(GenRelSerializationTests, test_one)
-{
-    MarshallNamespaceDescriptorBuilder mndb("testing");
-    ASSERT_TRUE(mndb.name() == "testing");
-    ASSERT_TRUE(mndb.events.size() == 0); 
-    ASSERT_TRUE(mndb.events.occupied_size() == 0);
 
-    MarshallEventDescriptorBuilder medb;
-
-    bool res;
-    Oid hassan_zero("hassan",0);
-    Oid hassan_zero_ord_collision("hassan1",0);
-    Oid hassan_zero_name_collision("hassan",1);
-
-    medb.Init(hassan_zero,mndb,res);
-    ASSERT_TRUE(res);
-
-    medb.AddEventType(  hassan_zero,
-                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
-                        res);
-    ASSERT_TRUE(res);
-    medb.AddEventType(  hassan_zero,
-                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
-                        res);
-    ASSERT_FALSE(res);
-    
-    medb.AddEventType(  hassan_zero_ord_collision,
-                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
-                        res);
-    ASSERT_FALSE(res);
-    medb.AddEventType(  hassan_zero_name_collision,
-                        EventTypeDescriptor(ENTRY_REQUIRED, VALUE_INT4, true),
-                        res);
-    ASSERT_FALSE(res);
-//    medb.
-}
-
-TEST(testing_marshall_building, blah)
+// descriptor_building_and_serialization
+TEST(descriptor_building_and_serialization , exhaustive_serialization_tests)
 {
     MarshallNamespaceDescriptorBuilder mndb("testing");
     ASSERT_TRUE(mndb.name() == "testing");
