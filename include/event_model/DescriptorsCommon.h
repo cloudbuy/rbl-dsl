49f1d16bb1da6cde666de8c9b13ee4cc68e71f7a
Hassan.Syed <hassan.syed@ukplc.net>
2011-04-19 12:21:42 +0100

    split out the monolithich descriptors into process specific and common base headers

---
+++ include/event_model/DescriptorsCommon.h
@@ -0,0 +1,52 @@
+#ifndef _EM_DESCRIPTORS_COMMON_H
+#define _EM_DESCRIPTORS_COMMON_H
+#include <event_model/Types.h>
+#include <boost/cstdint.hpp>
+#include "detail/OidContainer.h"
+
+namespace event_model
+{
+    using namespace primitives;
+   
+    // Common 
+    typedef OidConstrainedString<char, 32> OidName;
+    typedef OidType<OidName, boost::uint8_t> Oid;
+    
+    enum EVENT_DESCRIPTOR_QUALIFIER
+    {
+        ENTRY_UNINITIALIZED = 0,
+        ENTRY_REQUIRED,
+        ENTRY_OPTIONAL,
+    };
+    
+    class EventTypeDescriptor
+    {
+    public:
+        
+        EventTypeDescriptor();
+        explicit EventTypeDescriptor(   EVENT_DESCRIPTOR_QUALIFIER _qualifier,
+                                        VALUE_TYPE _type,
+                                        bool primitive_in);
+        
+        // no operator= or copy constructor as bitwise copy is fine
+
+        const bool is_primitive() const;
+        const EVENT_DESCRIPTOR_QUALIFIER qualifier() const;    
+        const VALUE_TYPE type() const;
+        
+        void set_is_primitive(bool _is_primitive);
+        void set_qualifier(EVENT_DESCRIPTOR_QUALIFIER _qualifier);
+        void set_type(VALUE_TYPE _type);
+ 
+        void serialize(SF::Archive & ar);
+    private:
+        EVENT_DESCRIPTOR_QUALIFIER qualifier_;
+        VALUE_TYPE  type_;
+        bool primitive_;
+    };
+    
+    typedef OidContainer<Oid, EventTypeDescriptor>  EventTypeContainer;
+    typedef OidContainerEntryType<Oid,EventTypeContainer> EventDescriptorBase;
+}
+#endif 
+
